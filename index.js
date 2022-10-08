95555f9f55f1d625d3638e92e1cecceb072a9af0
yanhackcode15 <iamyanhu@gmail.com>
2022-10-07 18:13:20 -0700

    one bug left

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-const dino_left = window.scrollX+document.querySelector('#dino').getBoundingClientRect().left;
-const dino_right = window.scrollX+document.querySelector('#dino').getBoundingClientRect().right;
+const dino = {}
+const cactus1 = {}
 
-var dino_top = 0;
-var dino_bottom = 0;
+dino.left = window.scrollX+document.querySelector('#dino').getBoundingClientRect().left;
+dino.right = window.scrollX+document.querySelector('#dino').getBoundingClientRect().right;
+dino.top = 0;
+dino.bottom = 0;
 
-const cactus1_top = window.scrollX+document.querySelector('#cactus1').getBoundingClientRect().top;
-const cactus1_bottom = window.scrollX+document.querySelector('#cactus1').getBoundingClientRect().bottom;
+cactus1.top = window.scrollX+document.querySelector('#cactus1').getBoundingClientRect().top;
+cactus1.bottom = window.scrollX+document.querySelector('#cactus1').getBoundingClientRect().bottom;
+cactus1.left = 0;
+cactus1.right = 0;
 
-// console.log('dino_left', dino_left)
-// console.log('dino_right', dino_right)
-// console.log('cac_top', cactus1_top)
-// console.log('cac_bottom', cactus1_bottom)
+
+var myInterval = setInterval("detectCollision()", 200)
+document.addEventListener('keypress', (event)=>{
+    if(event.key===" ") {start()}
+})
+
 
 function jump(){
     document.getElementById("dino").className="jump"
@@ -18,19 +24,34 @@ function jump(){
         document.getElementById("dino").className=""},2000);
 }
 
-function detectCollision(){
-    dino_top = window.scrollX+document.querySelector('#dino').getBoundingClientRect().top;
-    dino_bottom = window.scrollX+document.querySelector('#dino').getBoundingClientRect().bottom;
-    cactus1_left = window.scrollX+document.querySelector('#cactus1').getBoundingClientRect().left;
-    cactus1_right = window.scrollX+document.querySelector('#cactus1').getBoundingClientRect().right;
-    // console.log('dino_top', dino_top)
-    // console.log('dino_bottom', dino_bottom)    
-    // console.log('dino_top', dino_top)
-    // console.log('dino_bottom', dino_bottom)
-
-    if ((dino_bottom<=cactus1_top&&dino_bottom>=cactus1_bottom )||(cactus1_top<=dino_top&&cactus1_bottom>=dino_bottom)|| (dino_right>=cactus1_left&& dino_right<=cactus1_right)||(dino_left>=cactus1_left&& dino_left<=cactus1_right)||(cactus1_right<=dino_right&&cactus1_left>=dino_left)){
-        window.alert('dead')
-    }
-    
+function move(){
+    document.getElementById("cactus1").className="moveleft"
 }
-setInterval("detectCollision()", 500)
+
+function detectCollision(){
+    dino.top = window.scrollX+document.querySelector('#dino').getBoundingClientRect().top;
+    dino.bottom = window.scrollX+document.querySelector('#dino').getBoundingClientRect().bottom;
+    cactus1.left = window.scrollX+document.querySelector('#cactus1').getBoundingClientRect().left;
+    cactus1.right = window.scrollX+document.querySelector('#cactus1').getBoundingClientRect().right;
+    
+    
+    if ((dino.bottom>=cactus1.top&&dino.bottom<=cactus1.bottom)&&((dino.right>=cactus1.left&& dino.right<=cactus1.right)||(dino.right>=cactus1.right&& dino.left<=cactus1.left))) {
+       gameover();
+
+    } 
+}
+
+function gameover() {
+    clearInterval(myInterval);
+    document.getElementById('cactus1').className=""
+    document.getElementById('gameovertext').className=""
+}
+
+function start(){
+    jump();
+    if (document.getElementById('cactus1').className==="moveleft"){}
+    else move();
+    document.getElementById('gameovertext').className="hidden"
+
+}
+
